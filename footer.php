30ae0c9aa18c8e55c1b3b00af124cdd51162817e
Zsolt <zsolt.ardai@icloud.com>
2021-12-28 18:11:59 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,4 +1,7 @@
-        <footer>
+        <footer style="display: flex; flex-direction: row; justify-content: center;">
+            <a href="https://github.com/zsoltardai/websiteidea.epizy.com">
+                <img style="width: 120px; height: auto;" src="img/github.png" />
+            </a>
         </footer>
     </body>
 </html>
